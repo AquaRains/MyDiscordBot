328409d90cdb6ae10ee10aeba98751ad4e4f4b5c
Aquarains <keystroke915@naver.com>
2018-08-06 09:29:40 +0900

    ㅇㅇ

--- MyDiscordBot/webrequest.py
+++ MyDiscordBot/webrequest.py
@@ -2,8 +2,12 @@ import urllib
 import urllib.request
 
 def webReq():
-    f = urllib.request.urlopen('http://www.kma.go.kr/weather/forecast/mid-term-rss3.jsp?stnId=159')
-    print(f.read().decode('utf-8'))
+    #f = urllib.request.urlopen('http://www.kma.go.kr/weather/forecast/mid-term-rss3.jsp?stnId=159')
+    f = urllib.request.urlopen('http://www.kma.go.kr/wid/queryDFSRSS.jsp?zone=3114051000')
+
+    fstr = f.read().decode('utf-8')
+    fstrarr = fstr.split('\n')
+    print(fstrarr)
 
 
 def main():
